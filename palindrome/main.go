d6d749f5475c0b4f10a8757e054b5526670984ba
Saadyyyy <laodesaady12345@gmail.com>
2024-08-19 12:36:47 +0700

    palindrome done

--- palindrome/main.go
+++ palindrome/main.go
@@ -1,1 +1,25 @@
-package palindrome
+package main
+
+import "fmt"
+
+func main() {
+	var input string
+	fmt.Print("Masukkan kata: ")
+	fmt.Scan(&input)
+
+	if isPalindrome(input) {
+		fmt.Println("Kata tersebut adalah palindrome")
+	} else {
+		fmt.Println("Kata tersebut bukan palindrome")
+	}
+	main()
+}
+
+func isPalindrome(s string) bool {
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-i-1] {
+			return false
+		}
+	}
+	return true
+}
